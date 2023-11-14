7d6dd0655d3ed609b5ee2c27fe9cf4796174207c
unknown <ilia.shaidov@gmail.com>
2023-11-14 16:58:24 +0300

    first commit to the project

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,25 @@
 // Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
 // Напишите ответ здесь:
 
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num = 1000;
+	int count = 0;
+	do {
+		int temp = num;
+		while (temp > 0) {
+			if (temp % 10 == 3) {
+				count++;
+				break;
+			}
+			temp /= 10;
+		}
+		--num;
+	} while (num >= 3);
+	cout << count << endl;
+	return 0;
+}
+
 // Закомитьте изменения и отправьте их в свой репозиторий.
